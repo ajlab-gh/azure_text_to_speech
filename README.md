1229209488d2c354c85bb1911bd1f8a57e216513
AJLab-GH <ajammes.ftnt@gmail.com>
2024-07-16 14:41:52 -0600

    fixed dependencies for SSL version for TTS SDK

--- README.md
+++ README.md
@@ -6,11 +6,47 @@ pre-commit install
 
 # Introduction
 
-Turn text into lifelike spoken audio using OpenAI's TTS
+Turn text into lifelike spoken audio using Azure Speech Services's TTS
 
 ## Getting Started
 
 1. **Clone the repository**:
+
    ```bash
    git clone https://github.com/your-username/my-template-repo.git
    cd my-template-repo
+
+## Prerequisites
+
+Before you get started, here's a list of prerequisites:
+
+* A subscription key for the Speech service. See [Try the speech service for free](https://docs.microsoft.com/azure/cognitive-services/speech-service/get-started).
+* On Windows and Linux Python 3.6 or later needs to be installed. On Mac, minimum version for Python is 3.7. Downloads are available [here](https://www.python.org/downloads/).
+* The Python Speech SDK package is available for Windows (x64 and x86), Mac x64 (macOS X version 10.14 or later), Mac arm64 (macOS version 11.0 or later), and [specific Linux distributions and target architectures](https://docs.microsoft.com/azure/cognitive-services/speech-service/speech-sdk?tabs=linux).
+* On Ubuntu or Debian, run the following commands for the installation of required packages:
+  ```sh
+  sudo apt-get update
+  sudo apt-get install libssl-dev libasound2
+  ```
+
+* On RHEL or CentOS, run the following commands for the installation of required packages:
+  ```sh
+  sudo yum update
+  sudo yum install alsa-lib openssl python3
+  ```
+
+  * See also [how to configure RHEL/CentOS 7 for Speech SDK](https://docs.microsoft.com/azure/cognitive-services/speech-service/how-to-configure-rhel-centos-7).
+
+* On Windows you need the [Microsoft Visual C++ Redistributable for Visual Studio 2017](https://support.microsoft.com/help/2977003/the-latest-supported-visual-c-downloads) for your platform.
+
+## Get the Speech SDK Python Package
+
+**By downloading the Microsoft Cognitive Services Speech SDK, you acknowledge its license, see [Speech SDK license agreement](https://aka.ms/csspeech/license).**
+
+The Cognitive Services Speech SDK Python package can be installed from [pyPI](https://pypi.org/) using this command:
+
+```sh
+pip install azure-cognitiveservices-speech
+```
+
+Note: this tutorial will not work without changes for any version earlier than 1.7.0 of the SDK.
--- tts.py
+++ tts.py
@@ -1,35 +1,52 @@
+import azure.cognitiveservices.speech as speechsdk
 from pathlib import Path
-from openai import OpenAI
-import warnings
-
-# Ignore DeprecationWarning
-warnings.filterwarnings("ignore", category=DeprecationWarning)
+import os
 
 # Paths
 base_folder_path = Path(__file__).parent
 audio_folder_path = base_folder_path / "audio"
 text_folder_path = base_folder_path / "text"
 
+# Ensure directories exist
+audio_folder_path.mkdir(parents=True, exist_ok=True)
+text_folder_path.mkdir(parents=True, exist_ok=True)
+
 # File paths
 filename = "001"
 speech_file_path = audio_folder_path / f"{filename}.mp3"
 text_file_path = text_folder_path / f"{filename}.txt"
 
-# Text input
+# Enter the text you want to convert to speech!
 text_input = '''and that's why you don't mess with the three billy goats gruff!'''
 
 # Write text to file
 with open(text_file_path, 'w') as text_file:
     text_file.write(text_input)
 
-# Initialize OpenAI client
-client = OpenAI()
+# Creates an instance of a speech config with specified subscription key and service region.
+# Replace with your own subscription key and service region (e.g., "westus").
+speech_key, service_region = os.environ.get('SPEECH_KEY'), os.environ.get('SPEECH_REGION')
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
 
-# Generate Speech from Text
-response = client.audio.speech.create(
-  model="tts-1-hd",
-  voice="shimmer",
-  input= text_input
-)
+# Set the voice name, refer to https://aka.ms/speech/voices/neural for full list.
+speech_config.speech_synthesis_voice_name = "en-US-AvaMultilingualNeural"
 
-response.stream_to_file(speech_file_path)
+# Create an audio configuration that points to an audio file.
+audio_config = speechsdk.audio.AudioOutputConfig(filename=str(speech_file_path))
+
+# Creates a speech synthesizer using the specified audio config.
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+
+# Synthesizes the received text to speech.
+result = speech_synthesizer.speak_text_async(text_input).get()
+
+# Checks result.
+if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized to file [{}] for text [{}]".format(speech_file_path, text_input))
+elif result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+    print("Did you update the subscription info?")
